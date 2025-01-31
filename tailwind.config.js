7a35e210c9e8fb49b276f25d48a6441215bd8dea
jomirforhad <jomir988@gmail.com>
2025-01-31 16:18:22 +0600

    update my commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'custom-gradient': 'linear-gradient(180deg, rgb(255, 255, 255), rgb(239, 235, 227)100%)',
+        },
+      },
+    },
+  }
+  
+  
+  
